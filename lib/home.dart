2e77c2461cb5c368c40d28cb490c44d52270783b
vaidik2626 <21it047@charusat.edu.in>
2024-05-09 07:58:27 +0530

    final commit

--- lib/home.dart
+++ lib/home.dart
@@ -58,7 +58,6 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getRecipes("Ladoo");
   }
@@ -232,61 +231,7 @@ class _HomeState extends State<Home> {
                                   ),
                                 ),
                               );
-                            })),
-
-                // Container(
-                //   height: 100,
-                //   child: ListView.builder(
-                //       itemCount: reciptCatList.length,
-                //       shrinkWrap: true,
-                //       scrollDirection: Axis.horizontal,
-                //       itemBuilder: (context, index) {
-                //         return Container(
-                //             child: InkWell(
-                //           onTap: () {},
-                //           child: Card(
-                //               margin: EdgeInsets.all(20),
-                //               shape: RoundedRectangleBorder(
-                //                 borderRadius: BorderRadius.circular(18),
-                //               ),
-                //               elevation: 0.0,
-                //               child: Stack(
-                //                 children: [
-                //                   ClipRRect(
-                //                       borderRadius: BorderRadius.circular(18.0),
-                //                       child: Image.network(
-                //                         reciptCatList[index]["imgUrl"],
-                //                         fit: BoxFit.cover,
-                //                         width: 200,
-                //                         height: 250,
-                //                       )),
-                //                   Positioned(
-                //                       left: 0,
-                //                       right: 0,
-                //                       bottom: 0,
-                //                       top: 0,
-                //                       child: Container(
-                //                           padding: EdgeInsets.symmetric(
-                //                               vertical: 5, horizontal: 10),
-                //                           decoration: BoxDecoration(
-                //                               color: Colors.black26),
-                //                           child: Column(
-                //                             mainAxisAlignment:
-                //                                 MainAxisAlignment.center,
-                //                             children: [
-                //                               Text(
-                //                                 reciptCatList[index]["heading"],
-                //                                 style: TextStyle(
-                //                                     color: Colors.white,
-                //                                     fontSize: 28),
-                //                               ),
-                //                             ],
-                //                           ))),
-                //                 ],
-                //               )),
-                //         ));
-                //       }),
-                // )
+                  })),
               ],
             ),
           ),
